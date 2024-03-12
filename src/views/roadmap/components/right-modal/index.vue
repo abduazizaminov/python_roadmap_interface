b561085fe21a9859cca149f4f134febb8f00d3f0
abduazizaminov <abduazizaminov8@gmail.com>
2024-03-12 15:32:17 +0500

    create modal

--- src/views/roadmap/components/right-modal/index.vue
+++ src/views/roadmap/components/right-modal/index.vue
@@ -1,22 +1,32 @@
 <template>
   <div v-if="modalCondition">
-    <div class="fixed z-50 right-0 h-full top-0  bg-white p-5">
-      asdasd
+    <div class="fixed max-w-[500px] z-50 right-0 h-full top-0 bg-white p-5">
+      <div class="cursor-pointer mr-[-10px] mb-[10px] flex justify-end">
+        <div @click="closeModal()" class="cursor-pointer p-1 rounded-md hover:bg-[#121827] hover:bg-opacity-10">
+          <img src="@/assets/images/cancel.svg" alt="">
+        </div>
+      </div>
+      asdsdfdf asdfsadf asdfasdf asdfasdf 
     </div>
-    <div @click="closeModal()" class="fixed z-10 top-0 w-full h-full bg-black bg-opacity-30"></div>
+    <div @click="closeModal()" class="fixed z-10 top-0 w-full h-full bg-black bg-opacity-50"></div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, defineExpose } from 'vue';
 
-const modalCondition = ref(true)
+const modalCondition = ref<boolean>(true)
 
-const closeModal = ():any => {
+const closeModal = ():void => {
   modalCondition.value = false
 }
 
-const openModal = ():any => {
+const openModal = ():void => {
   modalCondition.value = true
 }
+
+defineExpose({
+  openModal,
+  closeModal
+});
 </script>
